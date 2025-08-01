fb9d3e4f703c0772a07c85c2b93aa6f58bceed30
Ruben civeira <rubenciveira@gmail.com>
2025-08-01 11:58:18 +0200

    fix: notify multiple creation kinds

--- src/Features/Oidc/User/Application/Listener/NotifyCreated.php
+++ src/Features/Oidc/User/Application/Listener/NotifyCreated.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 
 namespace Civi\Lughauth\Features\Oidc\User\Application\Listener;
 
+use Civi\Lughauth\Features\Access\User\Domain\Event\UserCreateEvent;
 use Civi\Lughauth\Features\Access\User\Domain\Event\UserRegisterAcceptedEvent;
 use Civi\Lughauth\Features\Notification\Message\Domain\Message;
 use Civi\Lughauth\Features\Notification\Message\Application\Usecase\SendMessage;
@@ -15,7 +16,7 @@ class NotifyCreated
     {
 
     }
-    public function __invoke(UserRegisterAcceptedEvent $auth)
+    public function __invoke(UserCreateEvent|UserRegisterAcceptedEvent $auth)
     {
         $message = new Message(
             targetName: 'Ruben Civeira',
